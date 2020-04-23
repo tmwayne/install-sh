0f996bca8aa080eb8ebd2ad013ac7756ce062dbc
Tyler <tylerwayne3@gmail.com>
2020-04-23 07:57:12 +0200

    add copy flag for hard copy isntead of symlink

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ Help() {
   echo
   echo "Options:"
   echo "  -h, --help                Print this help."
+  echo "  -c, --copy                Install a copy instead of a symlink."
   echo "  -i, --install-dir=DIR     Directory program is installed in."
   echo "  -p, --prog-name=NAME      Name to install program as."
   echo
@@ -28,6 +29,7 @@ for arg in "$@"; do
   shift
   case "$arg" in
     "--help")         set -- "$@" "-h" ;;
+    "--copy")         set -- "$@" "-c" ;;
     "--install-dir")  set -- "$@" "-i" ;;
     "--prog-name")    set -- "$@" "-p" ;;
     "--*")            "Invalid option: ${arg}"; exit 1;;
@@ -37,9 +39,10 @@ done
 
 # Command-line arguments
 OPTIND=1
-while getopts ":hf:i:p:" opt; do
+while getopts ":hcf:i:p:" opt; do
   case $opt in
     h) Help; exit 1 ;;
+    c) COPY=true ;;
     i) INSTALL_DIR=$( readlink -f $OPTARG ) ;;
     p) PROG_NAME=$OPTARG ;;
     \?) echo "Invalid option: -$OPTARG" >&2 ;;
@@ -94,33 +97,22 @@ if [ "$overwrite" != "y" ]; then
   exit 1
 fi
 
-# Make backup
-if [ $target_exists -eq 1 ]; then
-  mv "$TARGET" "$TARGET".bk
-fi
-
 # Install
-if ln -s "$FILE_NAME" "$TARGET"; then
-  successful_install=1
+successful_install=0
+if [ "$COPY" == "true" ]; then
+  if cp "$FILE_NAME" "$TARGET".new; then successful_install=1; fi
 else
-  successful_install=0
+  if ln -s "$FILE_NAME" "$TARGET".new; then successful_install=1; fi
 fi
 
 if [ $successful_install -eq 1 ]; then
   echo "Successfully installed ${PROG_NAME}!"
+  mv "$TARGET".new "$TARGET"
   exit_code=0
 else
   echo "${0}: error: failed to install target ..."
-  rm -f "$TARGET"
-  exit_code=1
+  rm -f "$TARGET".new
+  exit 1
 fi
 
-# Clean up
-if [ $target_exists -eq 1 ]; then
-  mv "$TARGET".bk "$TARGET"
-  rm -f "$TARGET".bk
-fi
-
-# Exit
-exit $exit_code
 
